897fb514ad9494e9ca321102876ac97db1cf23eb
Ilia Glazkov <ilia@ditto.com>
2013-08-24 20:34:55 +0400

    Initial code check-in.

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-exhausted
-=========
+Exhausted, a battery status widget for Awesome WM.
+=====
 
-Yet another awesome wm battery widget aimed for people, who are too tired to know anything about their laptop battery unless it's discharging.
+Exhausted is yet another battery status widget aimed for people, who are too
+tired to to know anything about their laptop battery unless it's discharging.
+
+The code is loosely based on [Vicious](http://git.sysphere.org/vicious/),
+[Delightful](http://scm.solitudo.net/gitweb/public/?p=delightful.git;a=summary),
+[Obvious](http://git.mercenariesguild.net/?p=obvious.git;a=summary),
+and `awful.widget.textclock`.
+
+Requirements
+-----
+- awesome 3.5
+- sys-power/acpi [URL](http://sourceforge.net/projects/acpiclient/)
+
+Usage
+-----
+Clone this repository into your `$XDG_CONFIG_HOME/awesome` (i.e. to
+awesome config dir). Add to the top of rc.lua:
+
+    local exhausted = require("exhausted")
+
+Then create a widget and add to wibox layout, just like any other widget:
+
+    mybattery = exhausted.widget()
+    right_layout:add(mybattery)
+
+And that's it! You're good to go.
+
+Screenshot
+-----
+![Screenshot][1]
+
+[1]: http://github.com/ioga/exhausted/raw/master/screenshot.png
+
+License
+-----
+Licensed under terms of GNU GPLv3.
+Copyright (C) 2013 Ilia Glazkov.
---
+++ init.lua
@@ -0,0 +1,52 @@
+-- Exhausted, the battery status widget for Awesome WM.
+--
+-- Author: Ilia Glazkov
+
+local io = {popen = io.popen}
+local math = {random = math.random, max = math.max, min = math.min}
+local string = string
+local tonumber = tonumber
+
+local capi = {timer = timer}
+local wibox = require('wibox')
+
+module('exhausted')
+
+local function colorize(s, color)
+  return string.format('<span foreground="%s"> ↯%s%%</span>', color, s)
+end
+
+local function get_battery_percent_left()
+  local f = io.popen('acpi -b')
+  local data = f:read('*all')
+  f:close()
+
+  local state, percent = data:match('Battery 0: ([%a]+), ([%d]+)%%')
+  if state ~= 'Discharging' then return end
+  return tonumber(percent)
+end
+
+local function get_color(percent)
+  local color_max = 255
+  local green = math.min(percent / 50, 1) * color_max
+  local red = (1 - math.max(percent / 50 - 1, 0)) * color_max
+  return string.format('#%x%x00', red, green)
+end
+
+function widget()
+  local w = wibox.widget.textbox()
+
+  local timer = capi.timer({timeout = math.random(5, 15)})
+  timer:connect_signal('timeout', function()
+    local text, percent = '', get_battery_percent_left()
+    if percent then
+      text = colorize(percent, get_color(percent))
+    end
+
+    w:set_markup(text)
+  end)
+  timer:start()
+  timer:emit_signal('timeout')
+
+  return w
+end
